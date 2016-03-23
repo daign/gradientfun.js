68ef78c2b056298ff7773e08d5effd9efb692166
Jakob Mischek <jakob.mischek@daign.de>
2016-03-23 11:44:15 +0100

    added minimum option for valueSlider

--- angular/js/value-slider.js
+++ angular/js/value-slider.js
@@ -4,13 +4,18 @@ app.directive( 'valueSlider', [ '$document', function ( $document ) {
 		templateUrl: 'html/value-slider.html',
 		scope: {
 			value: '=',
+			min: '=',
 			max: '=',
 			width: '='
 		},
 		link: function ( scope, element, attrs ) {
 
+			if ( scope.min === undefined ) {
+				scope.min = 0;
+			}
+
 			scope.left = function () {
-				return Math.round( scope.value * ( scope.width - 24 ) / scope.max );
+				return Math.round( ( ( scope.value - scope.min ) / ( scope.max - scope.min ) ) * ( scope.width - 16 ) );
 			};
 
 			scope.begin = function ( event, withJump ) {
@@ -19,7 +24,9 @@ app.directive( 'valueSlider', [ '$document', function ( $document ) {
 				event.stopPropagation();
 
 				if ( withJump ) {
-					scope.value = Math.max( 0, Math.min( scope.max, ( event.layerX - 10 ) * scope.max / ( scope.width - 24 ) ) );
+					scope.value = Math.round( Math.max( scope.min, Math.min( scope.max,
+						( ( event.layerX - 10 ) / ( scope.width - 24 ) ) * ( scope.max - scope.min ) + scope.min
+					)));
 				}
 
 				var startValue = scope.value;
@@ -33,7 +40,9 @@ app.directive( 'valueSlider', [ '$document', function ( $document ) {
 				function mousemove( event ) {
 					event.preventDefault();
 					event.stopPropagation();
-					scope.value = Math.max( 0, Math.min( scope.max, startValue + ( event.clientX - startPos ) * scope.max / ( scope.width - 24 ) ) );
+					scope.value = Math.round( Math.max( scope.min, Math.min( scope.max,
+						startValue + ( ( event.clientX - startPos ) / ( scope.width - 24 ) ) * ( scope.max - scope.min )
+					)));
 					scope.$apply();
 				}
 
